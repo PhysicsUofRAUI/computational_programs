1ddcb8c7dd342cdc934ec97f0c3c246008062c89
physicsuofraui <kodyrogers21@gmail.com>
2026-02-17 11:00:16 -0600

    add first try at a solution for the computer problem

---
+++ cp1_ch2_rhk/solver.py
@@ -0,0 +1,21 @@
+import math
+import sys
+
+def v_x(t):
+    return math.exp(-(t * t)) * (t + 10 * math.sin(math.pi*t))
+
+
+#
+# Take the arguments and simply go from the min to the 
+# max given the step specified
+#
+def main(max, min, step) :
+    t = min
+    while t < 10 :
+        velocity = v_x(t)
+        print("t = " + str(t) + "v_x = " + str(velocity))
+    
+    return
+
+if __name__ == '__main__':
+    main(int(sys.argv[1], 10), int(sys.argv[2], 10), float(sys.argv[3], 10))
